0b91e4d24aa0654cf22f18616c01f7daff3503c4
Jasiukajc <1068028@PE322-24.local>
2017-09-07 13:04:18 -0400

    first commit

---
+++ app/src/main/java/com/example/a1068028/notemaker_mk1/NoteActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.a1068028.notemaker_mk1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NoteActivityFragment extends Fragment {
+
+    public NoteActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
