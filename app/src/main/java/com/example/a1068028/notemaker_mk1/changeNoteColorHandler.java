97f7465b2769ea30780cc2854c5654b2630f97c0
nathanjay787 <nathanjay787@gmail.com>
2017-09-11 10:52:33 -0400

    added stuff

---
+++ app/src/main/java/com/example/a1068028/notemaker_mk1/changeNoteColorHandler.java
@@ -0,0 +1,15 @@
+package com.example.a1068028.notemaker_mk1;
+
+import android.view.View;
+
+/**
+ * Created by Resident on 2017-09-10.
+ */
+
+public class changeNoteColorHandler implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
